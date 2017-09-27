4d08baa53ab3ad928c2112fde012c25acd04d884
Keanu Code <keanu.code@gmail.com>
2017-09-27 17:15:28 +0800

    init spring boot guide

---
+++ src/main/java/idv/keanu/learning/springboot/guide/ServletInitializer.java
@@ -0,0 +1,13 @@
+package idv.keanu.learning.springboot.guide;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GuideApplication.class);
+	}
+
+}
